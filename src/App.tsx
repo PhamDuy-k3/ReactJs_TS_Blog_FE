849019d624845b81247fef43480f8ef84d7d2187
PhamDuy-k3 <phamdoduy123@gmail.com>
2024-07-09 20:53:32 +0700

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { RouterProvider } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
 import router from "./router";
 function App() {
   return <RouterProvider router={router} />;
